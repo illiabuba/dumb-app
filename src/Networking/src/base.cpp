65e8e35d828265dec0624c6ae6dbe3a822c69253
Hulk <bubnovskiy90@gmail.com>
2022-08-30 22:32:54 +0200

    linked boost lib. Created custom lib

---
+++ src/Networking/src/base.cpp
@@ -0,0 +1,7 @@
+#include <Networking/base.hpp>
+#include <boost/lambda/lambda.hpp>
+
+void PrintHello()
+{
+    std::cout << "HELLO NETWORKING";
+}
--- src/Server/server.cpp
+++ src/Server/server.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
-
+#include <Networking/base.hpp>
 int main(){
+    PrintHello();
     std::cout << "hello server";
     return 0;
 }
